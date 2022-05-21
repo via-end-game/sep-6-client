68ea6df07eed83205d111c3191a60e3160b5ba68
Eugeniu Racila <eugeniu.racila@gmail.com>
2022-05-21 20:18:51 +0200

    fix: cookie storage

--- pages/authorization.tsx
+++ pages/authorization.tsx
@@ -2,22 +2,34 @@ import { NextPage } from 'next';
 
 const submit = async (event: any) => {
   event.preventDefault();
-  const data = {
-    email: event.target.email.value,
-    password: event.target.password.value,
-  };
+  // const data = {
+  //   email: event.target.email.value,
+  //   password: event.target.password.value,
+  // };
 
-  const JSONdata = JSON.stringify(data);
-  console.log(JSONdata);
+  // const JSONdata = JSON.stringify(data);
+  // console.log(JSONdata);
 
-  const options = {
-    // The method is POST because we are sending data.
+  // const options = {
+  //   // The method is POST because we are sending data.
+  //   method: 'POST',
+  //   // Body of the request is the JSON data we created above.
+  //   body: JSONdata,
+  // };
+
+  const response = await fetch('http://localhost:3001/v1/auth/log-in', {
     method: 'POST',
-    // Body of the request is the JSON data we created above.
-    body: JSONdata,
-  };
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+    body: JSON.stringify({
+      email: 'ddd@aaa.com',
+      password: 'secret777',
+    }),
+  });
 
-  const response = await fetch('http://localhost:3001/v1/auth/log-in', options);
   console.log(response);
 };
 
@@ -25,9 +37,9 @@ const Authorization: NextPage = () => {
   return (
     <form onSubmit={submit}>
       <label htmlFor="email">Email:</label>
-      <input type="text" id="email" name="email" required />
+      <input type="text" id="email" name="email" />
       <label htmlFor="password">Password:</label>
-      <input type="text" id="password" name="password" required />
+      <input type="text" id="password" name="password" />
       <button type="submit">Submit</button>
     </form>
   );

6ae570603f1a2f51bc5d5dd528a4a8a2e8917f8f
Eugeniu Racila <eugeniu.racila@gmail.com>
2022-05-23 16:16:54 +0200

    fix: update domains for images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['image.tmdb.org', 'tmdb.org', 'themoviedb.org'],
+    domains: [
+      'image.tmdb.org',
+      'tmdb.org',
+      'themoviedb.org',
+      'www.themoviedb.org',
+    ],
   },
   reactStrictMode: true,
 };

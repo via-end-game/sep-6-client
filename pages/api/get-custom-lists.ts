3bfa21cf241d2e8f938d4685b23ec4907f9e4571
Eugeniu Racila <eugeniu.racila@gmail.com>
2022-05-30 22:05:29 +0200

    ci: solving google cloud api deny

--- pages/api/get-custom-lists.ts
+++ pages/api/get-custom-lists.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { getSession } from 'next-auth/react';
 
-const handler = async (req: NextApiRequest, res: NextApiResponse<void>) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse<any>) => {
   const session = await getSession({ req });
 
   if (session) {
@@ -15,9 +15,11 @@ const handler = async (req: NextApiRequest, res: NextApiResponse<void>) => {
     if (response.ok) {
       return res.status(200).json(await response.json());
     }
+
+    return res.status(response.status).json({ response });
   }
 
-  return res.status(401).send();
+  return res.status(401).send({ error: 'Not authenticated' });
 };
 
 export default handler;
